cdb6e2db2c74549ed6b5960f910ff0891ac717ab
Annalise Alvarez <afalvarez0821@gmail.com>
2019-07-06 21:23:13 -0400

    rolling ball challenge

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public class Lerper : MonoBehaviour
+    {
+        Vector3 pointA = new Vector3(0, 0, 0);
+        Vector3 pointB = new Vector3(1, 1, 1);
+        void Update()
+        {
+            transform.localPosition = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+        }
+    }
+}
---
+++ Challenge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text scoreText;
+    public Text lifeText;
+
+    private Rigidbody rb;
+    private int count;
+    private int score;
+    private int life;
+    private object player;
+    private object other;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        score = 0;
+        life = 3;
+        SetCountText();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+        rb.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score + 1;
+            SetCountText();
+        
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score - 1;
+            life = life - 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        lifeText.text = "Lives left: " + life.ToString();
+        scoreText.text = "Score: " + score.ToString();
+        countText.text = "Pickups collected: " + count.ToString();
+        if (count >= 20)
+        {
+            winText.text = "Congratulations, You Win!";
+        }
+        if (life <= 0)
+        {
+            winText.text = "You died! Try again?";
+            gameObject.CompareTag("Player");
+            gameObject.SetActive(false);
+        }
+
+    }
+}
+
